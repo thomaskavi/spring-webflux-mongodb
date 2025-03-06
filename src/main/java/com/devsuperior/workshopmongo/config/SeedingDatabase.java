a2e71b416f2ff3b50b900ccc3a7d8145ed4bf663
thomaskavi <thomaskavi2000@gmail.com>
2025-03-06 10:22:38 -0300

    Get user posts

--- src/main/java/com/devsuperior/workshopmongo/config/SeedingDatabase.java
+++ src/main/java/com/devsuperior/workshopmongo/config/SeedingDatabase.java
@@ -40,6 +40,10 @@ public class SeedingDatabase implements CommandLineRunner {
 		Flux<User> insertUsers = userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		insertUsers.subscribe();
 
+		maria = userRepository.searchEmail("maria@gmail.com").toFuture().get();
+		alex = userRepository.searchEmail("alex@gmail.com").toFuture().get();
+		bob = userRepository.searchEmail("bob@gmail.com").toFuture().get();
+
 		Post post1 = new Post(null, Instant.parse("2022-11-21T18:35:24.00Z"),
 				"Partiu viagem",
 				"Vou viajar para São Paulo. Abraços!", maria.getId(), maria.getName());
@@ -54,6 +58,9 @@ public class SeedingDatabase implements CommandLineRunner {
 
 		post2.addComment("Tenha um ótimo dia!",
 				Instant.parse("2022-11-23T18:35:24.00Z"), alex.getId(), alex.getName());
+
+		post1.setUser(userRepository.searchEmail("maria@gmail.com").block());
+		post2.setUser(userRepository.searchEmail("maria@gmail.com").block());
 
 		Flux<Post> insertPosts = postRepository.saveAll(Arrays.asList(post1, post2));
 		insertPosts.subscribe();
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -2,6 +2,7 @@ package com.devsuperior.workshopmongo.services;
 
 import java.time.Instant;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,5 +31,9 @@ public class PostService {
 	public Flux<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
 		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
 		return repository.fullSearch(text, minDate, maxDate).map(postFound -> new PostDTO(postFound));
+	}
+
+	public Flux<PostDTO> findByUser(String id) {
+		return repository.findByUser(new ObjectId(id)).map(post -> new PostDTO(post));
 	}
 }
